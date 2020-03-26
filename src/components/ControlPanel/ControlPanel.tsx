ef03098568dfc7d0adb20a264fd8845552afdd46
Красильников Роман Юрьевич <ryukrasilnikov@edu.hse.ru>
2020-03-26 16:08:36 +0500

    add url param

--- src/components/ControlPanel/ControlPanel.tsx
+++ src/components/ControlPanel/ControlPanel.tsx
@@ -1,51 +1,49 @@
-import React from 'react';
-import './ControlPanel.css';
-import Textbox from '../Textbox/Textbox';
+import React from "react";
+import "./ControlPanel.css";
+import Textbox from "../Textbox/Textbox";
 
 type controlPanelState = {
   textBoxValue: string;
-}
+};
 
 type controlPanelProps = {
   cbFromParent: any;
-}
+};
 
 // eslint-disable-next-line react/prefer-stateless-function
-class ControlPanel extends React.Component<controlPanelProps, controlPanelState> {
+class ControlPanel extends React.Component<
+  controlPanelProps,
+  controlPanelState
+> {
   constructor(props: any) {
     super(props);
-    this.state = { textBoxValue: '' };
+    this.state = { textBoxValue: "" };
   }
 
   getChildValue = (value: any) => {
     this.setState({
-      textBoxValue: value,
+      textBoxValue: value
     });
   };
 
   submitBtnClick = () => {
-    // const textBoxValue = [
-    //   { coords: { lat: 59.0058887, lng: 56.2708801 }, text: 'Text1' },
-    //   { coords: { lat: 58.0058887, lng: 56.2708801 }, text: 'Text2' },
-    //   { coords: { lat: 57.0058887, lng: 56.2708801 }, text: 'Text3' },
-    //   { coords: { lat: 56.0058887, lng: 56.2708801 }, text: 'Text4' },
-    // ];
     const { cbFromParent } = this.props;
 
     const { textBoxValue } = this.state;
-    console.log(textBoxValue);
-    fetch('https://whispering-sea-82070.herokuapp.com/')
-      .then(async (res: Response) => {
-        if (res && res.body && cbFromParent) {
-          const objResponse = await res.json();
-          const places = objResponse.places ? objResponse.places : [];
-          cbFromParent(places);
-        }
-      });
+
+    fetch(
+      `https://whispering-sea-82070.herokuapp.com?url=${textBoxValue}`
+    ).then(async (res: Response) => {
+      if (res && res.body && cbFromParent) {
+        const objResponse = await res.json();
+        const places = objResponse.places ? objResponse.places : [];
+        cbFromParent(places);
+      }
+    });
   };
 
   clearBtnClick = () => {
-    this.setState({ textBoxValue: '' });
+    this.setState({ textBoxValue: "" });
     const { cbFromParent } = this.props;
     if (cbFromParent) cbFromParent([]);
   };
@@ -53,13 +51,19 @@ class ControlPanel extends React.Component<controlPanelProps, controlPanelState>
   render() {
     return (
       <div className="controlPanel">
-        <Textbox
-          cbFromParent={this.getChildValue}
-        />
-        <button type="button" onClick={this.submitBtnClick} className="controlPanel__tb">
+        <Textbox cbFromParent={this.getChildValue} />
+        <button
+          type="button"
+          onClick={this.submitBtnClick}
+          className="controlPanel__tb"
+        >
           Submit
         </button>
-        <button type="button" onClick={this.clearBtnClick} className="controlPanel__tb">
+        <button
+          type="button"
+          onClick={this.clearBtnClick}
+          className="controlPanel__tb"
+        >
           Clear all
         </button>
       </div>
