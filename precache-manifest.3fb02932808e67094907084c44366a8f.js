1dc3753b48b0f9205d5698d7d2e1a00dc15abcb9
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-28 18:49:22 +0000

    Deploy savolkov/ips-map to github.com/savolkov/ips-map.git:gh-pages

---
+++ precache-manifest.3fb02932808e67094907084c44366a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7917ac2f7cffcf8e54f8440e9456c260",
+    "url": "/ips-map/index.html"
+  },
+  {
+    "revision": "81ea58498133f0aea795",
+    "url": "/ips-map/static/css/main.c727409c.chunk.css"
+  },
+  {
+    "revision": "cfee16680241b7cfe8f6",
+    "url": "/ips-map/static/js/2.1eb21a02.chunk.js"
+  },
+  {
+    "revision": "7df307703940913d279afef488923f08",
+    "url": "/ips-map/static/js/2.1eb21a02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81ea58498133f0aea795",
+    "url": "/ips-map/static/js/main.ee65d5a5.chunk.js"
+  },
+  {
+    "revision": "006e4b091c9e427f0a66",
+    "url": "/ips-map/static/js/runtime-main.40444f59.js"
+  }
+]);
